63c7a605ffe207435c43733198f02cc2369e32fa
amogh <am210633@dal.ca>
2020-11-29 19:25:58 -0400

    Completed get data endpoint and structure for add data

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pymongo
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 from pymongo import MongoClient
 
@@ -22,6 +22,12 @@ def getducks():
     ducks = duck_collection.find({}, {'_id': 0})
     duck_list = list(ducks)
     return jsonify(duck_list)
+
+@app.route("/addducks", methods=["GET","POST"])
+def addducks():
+ 	duckdata = request.get_json()
+ 	print("Duckdata = ", duckdata)
+ 	return jsonify("Duckdata added Successfully!")
 
 if __name__ == '__main__':
     app.run()
