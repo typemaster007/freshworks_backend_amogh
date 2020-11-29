888169b65eda883eac9d616d6ba8040a6bd46610
amogh <am210633@dal.ca>
2020-11-29 18:51:07 -0400

    Added MongoDB connection to app.py and getdata layout

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import pymongo
-from flask import Flask
+import json
+from flask import Flask, jsonify
 app = Flask(__name__)
+from pymongo import MongoClient
 
 
-MONGODB_URI = 'mongodb+srv://testuser:<password>@cluster0.mlfc9.mongodb.net/test'
+#MONGODB_URI = 'mongodb+srv://testuser:test123@cluster0.mlfc9.mongodb.net/test'
+
+client = pymongo.MongoClient("mongodb+srv://testuser:test123@cluster0.mlfc9.mongodb.net/test?retryWrites=true&w=majority")
+database = client.duckdata
+
+
 
 @app.route("/", methods=["GET"])
 def index():
-    app.logger.info('Welcome to Index')
     return "Hello index"
+
+@app.route("/getducks", methods=["GET"])
+def getducks():
+    duck_collection = database.feed
+    ducks = duck_collection.find({}, {'_id': 0})
+    duck_list = list(ducks)
+    return jsonify(duck_list)
 
 if __name__ == '__main__':
     app.run()
