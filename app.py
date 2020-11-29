5983f82465d83fb44e733b2ec111c2f4ec6681c8
amogh <am210633@dal.ca>
2020-11-29 19:53:13 -0400

    Completed Add Duck data to MongoDB functionality in app.py

--- app.py
+++ app.py
@@ -25,9 +25,39 @@ def getducks():
 
 @app.route("/addducks", methods=["GET","POST"])
 def addducks():
- 	duckdata = request.get_json()
- 	print("Duckdata = ", duckdata)
- 	return jsonify("Duckdata added Successfully!")
+ 	
+	duckdata = request.get_json()       #get the data from post request sent by client
+	username = duckdata["username"]
+	feedtime = duckdata["feedtime"]		#Exract the fields required for db insertion
+	food = duckdata["food"]
+	location = duckdata["location"]
+	number = duckdata["number"]
+	quantity = duckdata["quantity"]
+
+	print("Username = ", username)
+	if not duckdata:					
+	    err = {'ERROR': 'No data passed'}
+	    return jsonify(err)
+	
+	else:
+
+	    lastid = database.feed.find().sort([("id",-1)]).limit(1)	#Maintaining own increment counter in database hence adding this
+	    id = int(lastid [0]["id"]) + 1
+	    
+	    print(id)
+
+	    #Adding the extracted field to MongoDB Duck feed table
+	    database.feed.insert({
+	    	'id': str(id),
+	    	'username': username,
+	    	'feedtime': feedtime,
+	    	'food': food, 
+	    	'location': location,
+	    	'number' : number,
+	    	'quantity' : quantity
+	    	})
+
+	return jsonify("Duck data added Successfully!")
 
 if __name__ == '__main__':
     app.run()
